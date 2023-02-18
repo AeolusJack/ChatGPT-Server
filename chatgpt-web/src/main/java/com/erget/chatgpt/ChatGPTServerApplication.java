4db74ca2d26a543dbce21abf664f015444eab400
AeolusJack <865494582@qq.com>
2023-02-18 17:37:40 +0800

    项目初始化

---
+++ chatgpt-web/src/main/java/com/erget/chatgpt/ChatGPTServerApplication.java
@@ -0,0 +1,68 @@
+package com.erget.chatgpt;
+
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+/**
+ * ChatGPT服务平台启动类
+ */
+@SpringBootApplication(scanBasePackages = {"com.erget.chatgpt.**"},exclude={DataSourceAutoConfiguration.class})
+@EnableScheduling
+@Slf4j
+public class ChatGPTServerApplication {
+
+    public static void main(String[] args) {
+        Environment env = SpringApplication.run(ChatGPTServerApplication.class, args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Doc: \t\t{}://{}:{}/doc.html\n\t"+
+                        "SwaggerDoc: {}://{}:{}/swagger-ui.html\n\t"+
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                protocol,
+                hostAddress,
+                serverPort,
+                env.getActiveProfiles());
+
+    }
+}
